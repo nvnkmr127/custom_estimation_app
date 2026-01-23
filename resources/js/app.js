ec24437579c8a3be92b1d7b378f90b85a1b4af2e
Naveen <nvnkmr127@gmail.com>
2026-01-23 20:33:59 +0530

    cc3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import './bootstrap';
+import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 
-// Register Alpine plugins before Livewire/Alpine starts
-document.addEventListener('alpine:init', () => {
-    window.Alpine.plugin(collapse);
-});
-
-// window.Alpine will be automatically set by Livewire if not already present.
-// We do NOT call Alpine.start() here because Livewire 3+ handles it.
+window.Alpine = Alpine;
+Alpine.plugin(collapse);
+Alpine.start();
 
